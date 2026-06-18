caa54f592e3568d49244c436f445aa7650ca90ee
Rajdeep <rajdeepbiswasndp@gmail.com>
2026-06-18 17:38:12 +0530

    Try Catch Block

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def predict_premium(data : UserInput):
         "income_lpa": data.income_lpa,
         "occupation": data.occupation
     }
-    prediction = predict_output(user_input)
-    return JSONResponse(status_code=200, content={"predicted_category": prediction})
+    try:
+        prediction = predict_output(user_input)
+        return JSONResponse(status_code=200, content={"predicted_category": prediction})
+    except Exception as e:
+        return JSONResponse(status_code=500, content=str(e))
